15e14bf8ae8383f7f31b6abafed00d806bc745e7
Amos Augusto <amosaugustos@gmail.com>
2024-08-10 15:25:51 +0700

    feat: add live floating comments

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -15,8 +15,8 @@ import Loader from "./Loader";
 const CollaborativeRoom = ({
   roomId,
   roomMetadata,
+  users, currentUserType
 }: CollaborativeRoomProps) => {
-  const currentUserType = "editor";
 
   const [editing, setEditing] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -123,7 +123,7 @@ const CollaborativeRoom = ({
               </SignedIn>
             </div>
           </Header>
-          <Editor />
+          <Editor roomId={roomId} currentUserType={currentUserType}/>
         </div>
       </ClientSideSuspense>
     </RoomProvider>
